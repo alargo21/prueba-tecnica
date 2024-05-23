1cd3205d7a6f6e464105451537e663f0c958228d
Alejandro Largo <alejandro.largo97@gmail.com>
2024-05-23 17:27:06 +0200

    first commit and create pokemon endpoint

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class PokemonController extends Controller
+{
+    public function getPokemons(Request $request)
+    {
+        $limit = 50; // Número de resultados por página
+        $page = $request->query('page', 1); // Obtener el número de página de la query string, por defecto es 1
+        $offset = ($page - 1) * $limit; // Calcular el offset
+
+        $client = new Client();
+        // Hacer la solicitud a la PokeAPI con el límite y el offset calculados
+        $response = $client->get("https://pokeapi.co/api/v2/pokemon?limit=$limit&offset=$offset");
+        $pokemons = json_decode($response->getBody(), true);
+
+        $results = [];
+
+        // Procesar cada resultado de Pokémon para obtener detalles adicionales
+        foreach ($pokemons['results'] as $pokemon) {
+            $pokemonData = json_decode($client->get($pokemon['url'])->getBody(), true);
+            $results[] = [
+                'id' => $pokemonData['id'],
+                'name' => $pokemonData['name'],
+                'types' => array_map(fn($type) => $type['type']['name'], $pokemonData['types']),
+                'image' => $pokemonData['sprites']['front_default'],
+                'stats' => array_reduce($pokemonData['stats'], function ($carry, $stat) {
+                    $carry[$stat['stat']['name']] = $stat['base_stat'];
+                    return $carry;
+                }, [])
+            ];
+        }
+
+        // Devolver la respuesta en formato JSON
+        return response()->json([
+            'current_page' => $page,
+            'data' => $results,
+            'total' => $pokemons['count'],
+            'per_page' => $limit,
+            'last_page' => ceil($pokemons['count'] / $limit),
+        ]);
+    }
+}
